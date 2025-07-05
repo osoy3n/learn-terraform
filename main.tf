d0625292c80ffd23a75244446f61c237135849b3
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-07-05 16:49:22 -0500

    Beginning terraform

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = var.rg_name
+  location = "East US"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  description = "value for the resource group name"
+  type = string
+}
+
+variable "subscription_id" {
+  description = "value for the subscription ID"
+  type = string  
+}
